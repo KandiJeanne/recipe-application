789d284a396308c22047f7876800e782ccfd7577
Kandi Williams <kandijeannedesign@gmail.com>
2021-08-31 15:59:33 -0500

    fixed bug - new searches start on page 1

--- dist/index.436439df.js
+++ dist/index.436439df.js
@@ -12877,6 +12877,7 @@ const loadSearchResults = async function(query) {
                 image: rec.image_url
             };
         });
+        state.search.page = 1;
     } catch (err) {
         console.error(`${err} ooooo`);
         throw err;
